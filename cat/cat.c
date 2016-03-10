77db8676aa9d33445ac947fe71ec7fe8d059d285
Ilnar <sabirzyanovilnar@gmail.com>
2016-03-10 13:10:44 +0300

    fixed

--- cat/cat.c
+++ cat/cat.c
@@ -1,13 +1,16 @@
 #include <unistd.h>
 
+const ssize_t BUF_SIZE = 1024;
+
 int main(int argv, char ** argc) {
-    char buffer[1024];
-    int size = 0;
-    while ((size = read(STDIN_FILENO, buffer, 1024)) > 0) {
+    char buffer[BUF_SIZE];
+    ssize_t size = 0;
+    while ((size = read(STDIN_FILENO, buffer, BUF_SIZE)) > 0) {
         write(STDOUT_FILENO, buffer, size);
     }
     if (size < 0) {
         puts("Some error occured\n");
+        return size;
     }
     return 0;
 }
