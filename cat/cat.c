cfb74c97130fc8d9d3ff70f25f20ab6d75676c56
Ilnar <sabirzyanovilnar@gmail.com>
2016-03-15 02:43:19 +0300

    hw fixed

--- cat/cat.c
+++ cat/cat.c
@@ -6,7 +6,15 @@ int main(int argv, char ** argc) {
     char buffer[BUF_SIZE];
     ssize_t size = 0;
     while ((size = read(STDIN_FILENO, buffer, BUF_SIZE)) > 0) {
-        write(STDOUT_FILENO, buffer, size);
+        int count = 0;
+        while (count < size) {
+            int val = write(STDOUT_FILENO, buffer + count, size - count);
+            if (val < 0) {
+                puts("Can't write");
+                return val;
+            }
+            count += val;
+        }
     }
     if (size < 0) {
         puts("Some error occured\n");
