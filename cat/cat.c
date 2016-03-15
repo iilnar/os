42c0501ae2c292fcf32a3f48a48c0abb5d4fc031
Ilnar <sabirzyanovilnar@gmail.com>
2016-03-15 13:35:26 +0300

    defence01

--- cat/cat.c
+++ cat/cat.c
@@ -1,24 +1,46 @@
 #include <unistd.h>
-
+#include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
 const ssize_t BUF_SIZE = 1024;
 
-int main(int argv, char ** argc) {
+void cat(int from, int to) {
     char buffer[BUF_SIZE];
     ssize_t size = 0;
-    while ((size = read(STDIN_FILENO, buffer, BUF_SIZE)) > 0) {
-        int count = 0;
+    while ((size = read(from, buffer, BUF_SIZE)) > 0 || errno == EINTR) {
+        ssize_t count = 0;
         while (count < size) {
-            int val = write(STDOUT_FILENO, buffer + count, size - count);
+            ssize_t val = write(to, buffer + count, size - count);
             if (val < 0) {
+                if (errno == EINTR) {
+                    continue;
+                }
                 puts("Can't write");
-                return val;
+                return;
             }
             count += val;
         }
     }
     if (size < 0) {
-        puts("Some error occured\n");
-        return size;
+        puts("Can't read");
+    }
+    return;
+}
+
+int main(int argc, char ** argv) {
+    if (argc == 1) {
+        cat(0, 1);
+    } else {
+        int i = 1;
+        for (i = 1; i < argc; i++) {
+            size_t from = open(argv[i], O_RDONLY);
+            if (from == -1) {
+                puts("Can't open file ", argv[i]);
+                continue;
+            }
+            cat(from, 1);
+            close(from);
+        } 
     }
     return 0;
 }
